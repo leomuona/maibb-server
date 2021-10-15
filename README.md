bae06287fcddf6867237f43171837eef869080bb
Leo Muona <leo.muona@gmail.com>
2021-10-15 15:48:08 +0300

    go.mod filu

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 TODO: documentation
 
+```sh
+go run .
+```
+
 ## License
 
 This project is licensed under MIT License. See LICENSE file in the root directory of this project for details.
--- main.go
+++ main.go
@@ -11,6 +11,6 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
-
+	log.Println("starting server on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
