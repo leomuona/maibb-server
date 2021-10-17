ed8e55b94f439ac64cb287f625dff57380e6531f
Leo Muona <leo.muona@gmail.com>
2021-10-17 16:55:01 +0300

    jwt stuff

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/leomuona/maibb-server
 
 go 1.17
+
+require github.com/golang-jwt/jwt v3.2.2+incompatible
--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+
+	"github.com/leomuona/maibb-server/auth"
 )
 
 func main() {
+	jwtWrapper := auth.JwtWrapper{
+		SecretKey:       "topsekretlulz",
+		Issuer:          "AuthService",
+		ExpirationHours: 24,
+	}
+	generatedToken, err := jwtWrapper.GenerateToken("kikki.hiiri@gov.fi")
+	if err != nil {
+		return
+	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "Token: %s", generatedToken)
 	})
 	log.Println("starting server on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
